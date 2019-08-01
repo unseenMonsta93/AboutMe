a4d9cc14081ff723d6b0c914a83c87011074fde8
unseenMonsta93 <unseenMonsta945@gmail.com>
2019-07-31 20:15:54 -0500

    add changes

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,12 +7,27 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Kevin")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
 
 
 
@@ -21,7 +36,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun addNickname(view: View) {
+       fun addNickname(view: View) {
+
+           binding.apply{
+               myName?.nickname = nicknameEdit.text.toString()
+               NicknameText2.text = binding.nicknameEdit.text
+               invalidateAll()
+               binding.nicknameEdit.visibility = View.GONE
+               binding.doneButton.visibility = View.GONE
+               binding.NicknameText2.visibility = View.GONE
+           }
+
         val editText = findViewById<TextView>(R.id.Nickname_text2)
         val nicknameTextView = findViewById<TextView>(R.id.Nickname_text2)
 
